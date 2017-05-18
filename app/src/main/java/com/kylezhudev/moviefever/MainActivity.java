0f84e52b12be9313d6143df2d870d5b5ca6eee6a
Kyle Zhu <k.zhu@PARROT.BIZ>
2017-05-18 13:18:20 -0700

    Setting up recyclerView and adapter on Main

--- app/src/main/java/com/kylezhudev/moviefever/MainActivity.java
+++ app/src/main/java/com/kylezhudev/moviefever/MainActivity.java
@@ -5,15 +5,29 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.kylezhudev.moviefever.utilities.JsonUtil;
+import com.kylezhudev.moviefever.utilities.NetworkUtil;
 
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<String>{
-    TextView textView;
+    TextView mTextView;
+    ProgressBar mProgressBar;
+    TextView mTvError;
+    String mJsonString;
+    RecyclerView mRvMovie;
+    RecyclerView.Adapter mRvMovieAdapter;
     final String result = "{\"page\":1,\"results\":[{\"poster_path\":\"\\/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg\",\"adult\":false,\"overview\":\"The Guardians must fight to keep their newfound family together as they unravel the mysteries of Peter Quill's true parentage.\",\"release_date\":\"2017-04-24\",\"genre_ids\":[35,28,12,878],\"id\":283995,\"original_title\":\"Guardians of the Galaxy Vol. 2\",\"original_language\":\"en\",\"title\":\"Guardians of the Galaxy Vol. 2\",\"backdrop_path\":\"\\/aJn9XeesqsrSLKcHfHP4u5985hn.jpg\",\"popularity\":125.757946,\"vote_count\":1489,\"video\":false,\"vote_average\":7.6},{\"poster_path\":\"\\/2HjngGzVK3NTzptEtsT8E0Hi3ZB.jpg\",\"adult\":false,\"overview\":\"A live-action adaptation of Disney's version of the classic 'Beauty and the Beast' tale of a cursed prince and a beautiful young woman who helps him break the spell.\",\"release_date\":\"2017-03-16\",\"genre_ids\":[14,10749],\"id\":321612,\"original_title\":\"Beauty and the Beast\",\"original_language\":\"en\",\"title\":\"Beauty and the Beast\",\"backdrop_path\":\"\\/7QshG75xKCmClghQDU1ta2BTaja.jpg\",\"popularity\":97.735611,\"vote_count\":2422,\"video\":false,\"vote_average\":6.8}],\"total_results\":311205,\"total_pages\":15561}";
 
 
@@ -21,7 +35,18 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.tv_json_results);
+        mTextView = (TextView) findViewById(R.id.tv_json_results);
+        mProgressBar = (ProgressBar) findViewById(R.id.loading_indicator);
+        mTvError = (TextView) findViewById(R.id.tv_error);
+
+
+
+        if(mJsonString != null){
+
+        }
+
+
+
         String[] results = new String[result.length()];
         try {
            results = JsonUtil.getPosterPathFromJson(result);
@@ -30,7 +55,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         for(int i=0; i< results.length; i++){
-                textView.append(results[i] + "\n");
+                mTextView.append(results[i] + "\n");
         }
 
 
@@ -46,21 +71,40 @@ public class MainActivity extends AppCompatActivity
                 if(args == null){
                     return;
                 }
-
-
+                mProgressBar.setVisibility(View.VISIBLE);
             }
 
             @Override
             public String loadInBackground() {
+                URL url =  null;
+                JSONObject rawJson = null;
+                try {
+                    url = NetworkUtil.getPopMovieUrl();
+                    rawJson = NetworkUtil.getRawMovieResults(url);
+                    mJsonString = rawJson.toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-
-                return null;
+                return mJsonString;
             }
         };
     }
 
     @Override
     public void onLoadFinished(Loader<String> loader, String data) {
+        mProgressBar.setVisibility(View.INVISIBLE);
+        if(data == null){
+            mTvError.setVisibility(View.VISIBLE);
+     //TODO notify adapter data change
+        }else{
+
+        }
+
 
     }
 
@@ -68,4 +112,8 @@ public class MainActivity extends AppCompatActivity
     public void onLoaderReset(Loader<String> loader) {
 
     }
+
+
+
+
 }
--- app/src/main/java/com/kylezhudev/moviefever/utilities/NetworkUtil.java
+++ app/src/main/java/com/kylezhudev/moviefever/utilities/NetworkUtil.java
@@ -3,22 +3,33 @@ package com.kylezhudev.moviefever.utilities;
 
 import android.net.Uri;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NetworkUtil {
-    private final String KEY_API = "api_key";
-    private final String API_KEY = "";
+    private static final String KEY_API = "api_key";
+    private static final String API_KEY = "";
 
-    private final String IMG_BASE_URL = "https://image.tmdb.org/t/p";
-    private final String SEARCH_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
-    private final String KEY_SORT_BY = "sort_by";
-    private final String POPULARITY = "popularity.desc";
-    private final String IMG_SIZE = "w185";
+    private static final String IMG_BASE_URL = "https://image.tmdb.org/t/p";
+    private static final String SEARCH_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+    private static final String KEY_SORT_BY = "sort_by";
+    private static final String POPULARITY = "popularity.desc";
+    private static final String IMG_SIZE = "w185";
 
 
 
-    public URL getPopMovieUrl() throws MalformedURLException {
+    public static URL getPopMovieUrl() throws MalformedURLException {
         Uri builtUri = Uri.parse(SEARCH_BASE_URL)
                 .buildUpon()
                 .appendQueryParameter(KEY_API, API_KEY)
@@ -36,6 +47,39 @@ public class NetworkUtil {
                 .build();
         URL imgUrl = new URL(uri.toString());
         return imgUrl;
+    }
+
+    public JSONObject getPopMovieResults(URL url) throws IOException {
+        JSONObject rawMovieResults;
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+        try{
+            InputStream in = httpURLConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput){
+                rawMovieResults = new JSONObject(scanner.next());
+                return rawMovieResults;
+            } else{
+                return null;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            httpURLConnection.disconnect();
+        }
+    }
+
+    public static JSONObject getRawMovieResults(URL url) throws IOException, JSONException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        Response response = okHttpClient.newCall(request).execute();
+
+        JSONObject jsonResults = new JSONObject(response.body().toString());
+        return  jsonResults;
     }
 
 
